497c251da46679e69a2ad4a4710faa90300d798e
2omb Finance <96671285+2omb@users.noreply.github.com>
2022-02-06 19:15:55 -0500

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -76,7 +76,7 @@ const Footer = () => {
             >
               <img alt="youtube" src={YoutubeImage} className={classes.img} />
             </a>
-            <a href="https://discord.gg/pPpc9p5NS8" rel="noopener noreferrer" target="_blank" className={classes.link}>
+            <a href="https://discord.gg/2omb" rel="noopener noreferrer" target="_blank" className={classes.link}>
               <img alt="discord" src={DiscordImage} className={classes.img} />
             </a>
           </Grid>
