240bba7590f0e6788f25e85af9f48ee44d01780a
2omb Finance <2ombfinance@protonmail.com>
2022-02-11 12:01:50 -0800

    Update Cemetery.js

--- src/views/Rebates/Cemetery.js
+++ src/views/Rebates/Cemetery.js
@@ -98,7 +98,7 @@ const Cemetery = () => {
           {!!account ? (
             <>
               <Typography color="textPrimary" align="center" variant="h3" gutterBottom style={{ marginBottom: '40px' }}>
-                3DAO - Tresury * Still under Mantance 50% done *
+                3DAO - Treasury * Still under Mantance 50% done *
               </Typography>
               <Box mt={2}>
                 <Grid container justify="center" spacing={3}>
